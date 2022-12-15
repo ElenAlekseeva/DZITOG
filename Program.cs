bb2a1eba91834c1969025bddacb036233a7b48f5
Elena <lady.aleck98@icloud.com>
2022-12-15 21:34:42 +0300

    Четвертый

--- Program.cs
+++ Program.cs
@@ -4,4 +4,8 @@
                 (from ELEMENT in arrayInput 
                  where ELEMENT.Length <= 3 select ELEMENT)
                 .ToArray();
-                
+                        for (int i = 0; i < newArray.Length; i++)
+            {
+                Console.WriteLine("["+ i+ "]"+newArray[i]);
+            }
+            Console.ReadKey();
